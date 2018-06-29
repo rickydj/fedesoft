0fdbf220d6348dfebe62960379f3e6ac3472e9a6
Jorger Sanchez <orbitalsoftwareco@gmail.com>
2018-06-28 21:14:54 -0500

    aCTUALIZACION dISEÑO CON ANGULAR 1

--- html/js/worldcup.js
+++ html/js/worldcup.js
@@ -2,7 +2,19 @@ angular.module('worldcup', [])
     .controller("Mundialista", function ($scope, $http) {
         var mundial = this;
         mundial.datos = [];
-        $http.get('json/worldcup.json').then(function (response) {
-            mundial.datos = response.data;
-        })
+        $http.get('json/worldcup.json').then(async function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+                datos = response.data[i];
+                datos.home_team.code2Digits = await mundial.getCode(datos.home_team.code);
+                datos.away_team.code2Digits = await mundial.getCode(datos.away_team.code);
+                mundial.datos.push(datos);
+            }
+        });
+        mundial.getCode = async function (code) {
+            let info = await $http.get(`https://restcountries.eu/rest/v2/alpha/${code}`);
+            if(info.status==200){
+                return info.data.alpha2Code;    
+            }
+            return "";
+        };
     })
